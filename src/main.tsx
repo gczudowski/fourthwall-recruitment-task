817b5b55f2b74e765279a25070a3c932d9d9c8e5
Grzegorz <gczudowski@gmail.com>
2025-01-25 23:31:55 +0100

    wip

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 5 * 60 * 1000,
+      staleTime: 5 * 60 * 1000, // 5 minutes
       retry: 3,
     },
   },
