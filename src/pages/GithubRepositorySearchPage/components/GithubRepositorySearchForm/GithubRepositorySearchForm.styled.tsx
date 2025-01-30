e293135a3dd5fb9718824d29b509fa362f11a571
Grzegorz <gczudowski@gmail.com>
2025-01-30 13:21:42 +0100

    fix input position

--- src/pages/GithubRepositorySearchPage/components/GithubRepositorySearchForm/GithubRepositorySearchForm.styled.tsx
+++ src/pages/GithubRepositorySearchPage/components/GithubRepositorySearchForm/GithubRepositorySearchForm.styled.tsx
@@ -1,8 +1,13 @@
 import styled from 'styled-components'
+import { media } from '../../../../config/media'
 
 export const SearchFormWrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 12px;
   margin: 0 16px;
+
+  ${media.tabletAndDesktop`
+    margin: 0 auto;
+  `}
 `
